a3ae507f24c7ca97c65f927ecb854cb74811517d
PUNEET MANDIL <puneetmandil.mec18@itbhu.ac.in>
2020-06-06 18:30:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,10 +83,11 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+
   
 end
 
@@ -108,6 +109,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for]=alans_new_info
      
      
 end
